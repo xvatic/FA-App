8b3984524ba8cacef69ca40c8b5bff4307013635
xvatic <glazkovE@gmail.com>
2020-07-12 23:58:39 +0300

    Initial Commit

---
+++ FA App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FA App
+//
+//  Created by Евгений on 7/12/20.
+//  Copyright © 2020 Евгений. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

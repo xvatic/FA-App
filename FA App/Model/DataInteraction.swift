98a5969e5b10385d4a10ee9437d7ce96d66c6baf
xvatic <glazkovE@gmail.com>
2020-07-13 00:34:09 +0300

    web interaction module introduced

---
+++ FA App/Model/DataInteraction.swift
@@ -0,0 +1,15 @@
+//
+//  DataInteraction.swift
+//  FA App
+//
+//  Created by Евгений on 7/13/20.
+//  Copyright © 2020 Евгений. All rights reserved.
+//
+
+import Foundation
+
+var articles: [Article] = []
+
+func loadTopRatedArticles() {
+    
+}
---
+++ FA App/Model/DataModel.swift
@@ -0,0 +1,16 @@
+//
+//  DataModel.swift
+//  FA App
+//
+//  Created by Евгений on 7/13/20.
+//  Copyright © 2020 Евгений. All rights reserved.
+//
+
+import Foundation
+
+//STRUCT ONLY FOR ENTRY (HYPED RECIPIES)
+
+struct Article {
+
+    
+}

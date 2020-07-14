92637017041835d3891541cb6a1b42dfff924acf
xvatic <glazkovE@gmail.com>
2020-07-15 02:25:07 +0300

    base parse function added

--- FA App/Model/DataInteraction.swift
+++ FA App/Model/DataInteraction.swift
@@ -11,7 +11,7 @@ import Foundation
 var articles: [Article] = []
 
 func loadTopRatedArticles() {
-    let url = URL(string: "http://newsapi.org/v2/everything?q=bitcoin&from=2020-06-14&sortBy=publishedAt&apiKey=API_KEY")
+    let url = URL(string: "http://newsapi.org/v2/everything?q=bitcoin&from=2020-06-14&sortBy=publishedAt&apiKey=b0579ddfdda747a2833a48fde23b2087")
     let session = URLSession(configuration: .default)
     let dowloadTask = session.downloadTask(with: url!) { (urlFile, responce, error) in
         if urlFile != nil {
@@ -19,9 +19,30 @@ func loadTopRatedArticles() {
             let urlPath = URL(fileURLWithPath: path)
             try? FileManager.default.copyItem(at: urlFile!, to: urlPath)
             print(urlPath)
+            
+            parseTopRatedArticles()
+            
+            print(articles.count)
         }
     }
     
     dowloadTask.resume()
+    
+}
+
+func parseTopRatedArticles() {
+    let path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0]+"/data.json"
+    let urlPath = URL(fileURLWithPath: path)
+    
+    let data = try? Data(contentsOf: urlPath)
+    let rootDictionary = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! Dictionary<String, Any>
+    let array = rootDictionary!["articles"] as! [Dictionary<String, Any>]
+    var returnArray: [Article] = []
+    
+    for dict in array {
+        let newArticle = Article(dictionary: dict)
+        returnArray.append(newArticle)
+    }
+    articles = returnArray
     
 }
--- FA App/Model/DataModel.swift
+++ FA App/Model/DataModel.swift
@@ -11,6 +11,26 @@ import Foundation
 //STRUCT ONLY FOR ENTRY (HYPED RECIPIES)
 
 struct Article {
-
+    var author: String
+    var title: String
+    var description: String
+    var url: String
+    var urlToImage: String
+    var publishedAt: String
+    var sourceName: String
+    init (dictionary: Dictionary<String, Any>) {
+        author = dictionary["author"] as? String ?? ""
+        title = dictionary["title"] as? String ?? ""
+        description = dictionary["description"] as? String ?? ""
+        url = dictionary["url"] as? String ?? ""
+        urlToImage = dictionary["urlToImage"] as? String ?? ""
+        publishedAt = dictionary["publishedAt"] as? String ?? ""
+        
+        
+        sourceName = (dictionary["source"] as? Dictionary<String, Any> ?? ["":""])["name"] as? String ?? ""
+        
+    }
+    
+    
     
 }

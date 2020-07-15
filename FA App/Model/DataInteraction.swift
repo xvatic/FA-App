54340476497560ceb9865f8267f508af5a637b9f
xvatic <glazkovE@gmail.com>
2020-07-15 23:47:58 +0300

    base platform interaction added

--- FA App/Model/DataInteraction.swift
+++ FA App/Model/DataInteraction.swift
@@ -9,16 +9,22 @@
 import Foundation
 
 var articles: [Article] = []
+var urlToData: URL {
+    let path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0]+"/data.json"
+    let urlPath = URL(fileURLWithPath: path)
+    return urlPath
+}
 
 func loadTopRatedArticles() {
-    let url = URL(string: "http://newsapi.org/v2/everything?q=bitcoin&from=2020-06-14&sortBy=publishedAt&apiKey=b0579ddfdda747a2833a48fde23b2087")
+    let url = URL(string: "http://9628a5b04f59.ngrok.io/weatherforecast")
     let session = URLSession(configuration: .default)
     let dowloadTask = session.downloadTask(with: url!) { (urlFile, responce, error) in
         if urlFile != nil {
-            let path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0]+"/data.json"
-            let urlPath = URL(fileURLWithPath: path)
-            try? FileManager.default.copyItem(at: urlFile!, to: urlPath)
-            print(urlPath)
+            
+            try? FileManager.default.removeItem(at: urlToData)
+            
+            try? FileManager.default.copyItem(at: urlFile!, to: urlToData)
+            print(urlToData)
             
             parseTopRatedArticles()
             
@@ -30,19 +36,60 @@ func loadTopRatedArticles() {
     
 }
 
+func uploadComment() {
+    let parameters = ["username": "daddy", "comment":"Hello"]
+    
+    guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return}
+    
+    request.httpBody = httpBody
+    
+    let session = URLSession.shared
+    session.dataTask(with: request) { (data, response, error) in
+        if let response = response {
+            print(response)
+        }
+        if let data = data {
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+        }
+    }.resume()
+    
+        
+}
+
 func parseTopRatedArticles() {
-    let path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0]+"/data.json"
-    let urlPath = URL(fileURLWithPath: path)
     
-    let data = try? Data(contentsOf: urlPath)
-    let rootDictionary = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! Dictionary<String, Any>
-    let array = rootDictionary!["articles"] as! [Dictionary<String, Any>]
-    var returnArray: [Article] = []
     
-    for dict in array {
-        let newArticle = Article(dictionary: dict)
-        returnArray.append(newArticle)
+    let data = try? Data(contentsOf: urlToData)
+    if data == nil {
+        return
     }
-    articles = returnArray
+   
+    let rootDictionaryAny = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments)
+    if rootDictionaryAny == nil {
+        return
+    }
+    let rootDictionary = rootDictionaryAny as? Dictionary<String, Any>
+    if rootDictionary == nil {
+        return
+    }
+    
+    if let array = rootDictionary!["articles"] as? [Dictionary<String, Any>] {
+        var returnArray: [Article] = []
+        
+        for dict in array {
+            let newArticle = Article(dictionary: dict)
+            returnArray.append(newArticle)
+        }
+        articles = returnArray
+    }
     
 }
